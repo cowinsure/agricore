d15a38f04370f8879181191908750a7cdb05bb0a
cowinsure <cowinsure@gmail.com>
2026-02-09 13:57:02 +0600

    config updated

--- next.config.ts
+++ next.config.ts
@@ -4,24 +4,13 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
 
   images: {
-    domains: ['website-v1.insurecow.com'],
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'assets.example.com',
-        port: '',
-        pathname: '/account123/**',
-        search: '',
-      },
-      {
-        protocol: 'https',
         hostname: 'website-v1.insurecow.com',
-        port: '',
-        pathname: '/media/**',
-        search: '',
+        pathname: '/**',
       },
     ],
-    // Adjusted this line
   },
   /* config options here */
 };
